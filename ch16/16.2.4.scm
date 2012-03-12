89f3f2fa273bf934c58f9115c76c1132402139ce
Jeong-Min Lee <falsetru@gmail.com>
2012-03-12 22:52:24 +0900

    Solve 16.2.4

---
+++ ch16/16.2.4.scm
@@ -0,0 +1,7 @@
+(define-struct dir (name size))
+
+(define Text (make-dir 'Text (list 'part1 'part2 'part3)))
+(define Code (make-dir 'Code (list 'hang 'draw)))
+(define Docs (make-dir 'Docs (list 'read!)))
+(define Libs (make-dir 'Libs (list Code Docs)))
+(define TS   (make-dir 'TS (list Text Libs 'read!)))
