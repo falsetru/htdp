1184c79de2a5357a7fbe6470f4a51718f22f5c12
Jeong-Min Lee <falsetru@gmail.com>
2012-03-19 19:29:04 +0900

    Give a more suitable name.

--- ch17/17.7.1.scm
+++ ch17/17.7.1.scm
@@ -1,9 +1,9 @@
 (define-struct add (left right))
 (define-struct mul (left right))
-(define-struct function (name arg))
+(define-struct call (name arg))
 
 ; (f (+ 1 1))
-(make-function 'f (make-add 1 1))
+(make-call 'f (make-add 1 1))
 
 ; (* 3 (g 2))
-(make-mul 3 (make-function 'g 2))
+(make-mul 3 (make-call 'g 2))
