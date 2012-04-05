3f0ca96f1eaafe90d51ba8211b58fbee62287c9c
Jeong-Min Lee <falsetru@gmail.com>
2012-04-06 01:38:34 +0900

    20.2.2

---
+++ 20/20.2.2.scm
@@ -0,0 +1,3 @@
+; 1. sort: (listof number) (number number->boolean) -> (listof number)
+; 2. map: (number->number) (listof number) -> (listof number)
+; 3. project: (listof symbol) ((listof symbol)->symbol) -> (listof symbol) 
