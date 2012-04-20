1f2d96da57ff9f2abf58d106ab56db78291f1100
Jeong-Min Lee <falsetru@gmail.com>
2012-04-20 10:27:18 +0900

    Evaluation by hand.

--- 26/26.3.3.scm
+++ 26/26.3.3.scm
@@ -7,3 +7,14 @@
 
 (time (gcd-generative 101135853 45014640))
 ; cpu time: 0 real time: 0 gc time: 0
+
+
+; (gcd-generative 101135853 45014640) =
+; (gcd-generative 45014640 11106573) =
+; (gcd-generative 11106573 588348) =
+; (gcd-generative 588348 516309) =
+; (gcd-generative 516309 72039) =
+; (gcd-generative 72039 12036) =
+; (gcd-generative 12036 11859) =
+; (gcd-generative 11859 117) =
+; (gcd-generative 117 0) = 117
