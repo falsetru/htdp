f9c6c9e83bb3af4faf38661de38904cd0b782c87
Jeong-Min Lee <falsetru@gmail.com>
2012-05-01 19:50:41 +0900

    30.2.1

---
+++ 30/30.2.1.scm
@@ -0,0 +1,40 @@
+(define SimpleG 
+  '((A B)
+    (B C)
+    (C E)
+    (D E)
+    (E B)
+    (F F))) 
+
+(define (contains x xs)
+  (cons? (memq x xs)))
+
+(define (neighbor a-node sg)
+  (cond
+    [(empty? sg) (error "neighbor: impossible")]
+    [else (cond
+	    [(symbol=? (first (first sg)) a-node)
+	     (second (first sg))]
+	    [else (neighbor a-node (rest sg))])]))
+
+(define (route-exists2? orig dest sg)
+  (local ((define (re-accu? orig dest sg accu-seen)
+            (cond
+              [(symbol=? orig dest) true]
+              [(contains orig accu-seen) false]
+              [else (re-accu? (neighbor orig sg) dest sg (cons orig accu-seen))]))) 
+    (re-accu? orig dest sg empty)))
+
+
+(require rackunit)
+(require rackunit/text-ui)
+
+(define foo-tests
+  (test-suite
+   "Test for foo"
+
+   (check-equal? (route-exists2? 'A 'C SimpleG) true)
+   (check-equal? (route-exists2? 'C 'D SimpleG) false)
+   ))
+
+(exit (run-tests foo-tests))
