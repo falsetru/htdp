e24cefa77530f400c015e591b60a599270ff2318
Jeong-Min Lee <falsetru@gmail.com>
2012-05-16 23:02:36 +0900

    37.2.1

---
+++ 37/37.2.1.scm
@@ -0,0 +1,16 @@
+#lang racket
+
+(define (make-status-word word)
+  (map (lambda (c) '_) word))
+
+(require rackunit)
+(require rackunit/text-ui)
+
+(define make-status-word-tests
+  (test-suite
+   "Test for make-status-word"
+
+   (check-equal? (make-status-word '(h e l l o ))'(_ _ _ _ _))
+   ))
+
+(exit (run-tests make-status-word-tests))
