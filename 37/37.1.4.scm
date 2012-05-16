68fd58ad3857d8fb274a4ea2dd2912a8378a2819
Jeong-Min Lee <falsetru@gmail.com>
2012-05-16 22:27:50 +0900

    37.1.4 - no test!

---
+++ 37/37.1.4.scm
@@ -0,0 +1,33 @@
+#lang racket
+
+(define COLORS
+  '(black white red blue green gold pink orange purple navy))
+(define COL# (length COLORS))
+
+(define target1 (first COLORS))
+(define target2 (first COLORS))
+(define guess-count 0)
+
+(define (random-pick xs)
+  (list-ref xs (random (length xs))))
+
+(define (master)
+  (begin (set! target1 (random-pick COLORS))
+         (set! target2 (random-pick COLORS))
+         (set! guess-count 0)))
+
+(define (check-color guess1 guess2 target1 target2)
+  (cond [(and (symbol=? guess1 target1 guess2 target2)) 'Perfect!]
+        [(or (symbol=? guess1 target1 guess2 target2)) 'OneColorAtCorrectPosition]
+        [(or (symbol=? guess1 target2 guess2 target1)) 'OneColorOccurs]
+        [else 'NothingCorrect]))
+
+(define (master-check guess1 guess2)
+  (begin
+    (set! guess-count (add1 guess-count))
+    (local ((define result (list (check-color guess1 guess2 target1 target2) guess-count)))
+           (if (symbol=? result 'Perfect)
+             (master)
+             (void))
+           result)
+    ))
