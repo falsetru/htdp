6511be72185f255fcc230dd9ca04d00fd13fcfcf
Jeong-Min Lee <falsetru@gmail.com>
2012-05-19 01:19:51 +0900

    Split html file. Read html file every time.

--- misc/www-chat.scm
+++ misc/www-chat.scm
@@ -4,7 +4,8 @@
 
 (require web-server/servlet-env
          web-server/http/bindings
-         web-server/http/response-structs)
+         web-server/http/response-structs
+         racket/file)
 
 (define channels empty)
 
@@ -34,114 +35,12 @@
                       empty
                       (list #"" (string->bytes/utf-8 (format "~s" v))))])))
 
-(define default-page
-  (format
-   #<<EOF
-<html>
-<head>
-<script>
 
-var XMLHttpFactories = [
-    function () {return new XMLHttpRequest()},
-    function () {return new ActiveXObject("Msxml2.XMLHTTP")},
-    function () {return new ActiveXObject("Msxml3.XMLHTTP")},
-    function () {return new ActiveXObject("Microsoft.XMLHTTP")}
-];
-var createXMLHTTPObject = function() {
-    var xmlhttp = false;
-    for (var i=0;i<XMLHttpFactories.length;i++) {
-            try {
-                    xmlhttp = XMLHttpFactories[i]();
-            }
-            catch (e) {
-                    continue;
-            }
-            break;
-    }
-    return xmlhttp;
-}
-
-var post = function(msg) {
-    var req = createXMLHTTPObject();
-    if (!req) return;
-    req.open("POST", "/comet", true);
-    req.setRequestHeader('Content-type','application/x-www-form-urlencoded');
-    req.send("msg=" + encodeURIComponent(msg));
-}
-
-var sendmsg = function() {
-    var name = document.getElementById('name');
-    var msg = document.getElementById('msg');
-    var n = name.value.replace(/^\s+|\s+$/g, '');
-    var m = msg.value.replace(/^\s+|\s+$/g, '');
-    if (! m)
-       return;
-
-    post(n + ': ' + m);
-    msg.value = '';
-    msg.focus();
-}
-
-var sendmsg2 = function(event) {
-    if (event.keyCode == 13)
-        sendmsg();
-}
-
-var startComet = function() {
-    // http://www.quirksmode.org/js/xmlhttp.html//
-    // XMLHttpRequest wrapper.  Transparently restarts the request
-    // if a timeout occurs.
-    var sendRequest = function(url,callback,postData) {
-        var req = createXMLHTTPObject(), method, TIMEOUT = ~s;
-        if (!req) return;
-        method = (postData) ? "POST" : "GET";
-        req.open(method,url,true);
-        if (postData) {
-            req.setRequestHeader('Content-type','application/x-www-form-urlencoded');
-        }
-        req.onreadystatechange = function () {
-                if (req.readyState !== 4) return;
-                if (req.status === 200 && req.statusText === 'Try again') {
-                    req.abort();
-                    setTimeout(function() { sendRequest(url, callback, postData); }, 0);
-                    return;
-                }
-                if (req.status !== 200 && req.status !== 304) {
-                    return;
-                }
-                callback(req);
-        }
-        if (req.readyState === 4) return;
-        req.send(postData);
-    }
-
-    sendRequest(
-        "/comet",
-        function(req) {
-            document.body.appendChild(document.createTextNode(req.responseText));
-            document.body.appendChild(document.createElement("br"));
-            setTimeout(startComet, 0);
-        },
-        "comet=t"
-    );
-};
-
-var whenLoaded = function() {
-    document.getElementById("name").focus();
-    setTimeout(startComet, 0);
-};
-
-</script>
-</head>
-<body onLoad="whenLoaded()">
-    <div>
-        <input type="text" id="name" value="nobody" size="5" />
-        <input type="text" id="msg" onkeypress="sendmsg2(event)" />
-        <button id="btn" onclick="sendmsg()">Send</button>
-    </div>
-</body>
-</html>
-EOF
+(define-values (parent-dir filename be-dir?)
+               (split-path (find-system-path 'run-file)))
+(define html-file-path (build-path parent-dir "www-chat.html"))
+(define (default-page)
+  (format (file->string html-file-path)
    *alarm-timeout*))
 
 ;; handle-default: request -> response
@@ -150,7 +49,7 @@ EOF
                  (current-seconds)
                  TEXT/HTML-MIME-TYPE
                  empty
-                 (list #"" (string->bytes/utf-8 default-page))))
+                 (list #"" (string->bytes/utf-8 (default-page)))))
 
 ;; handle-msg: request -> response
 (define (handle-msg req)
