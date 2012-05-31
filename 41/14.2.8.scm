2fce42216e292733ebd6468d60dc862bb4ca6038
Jeong-Min Lee <falsetru@gmail.com>
2012-05-31 23:40:10 +0900

    14.2.8

---
+++ 41/14.2.8.scm
@@ -0,0 +1,26 @@
+#lang racket
+
+(define-struct animal (name breakfast dinner) #:transparent #:mutable)
+(define (create-animal name) (make-animal name false false))
+(define (feed-animal animal time)
+  (cond [(symbol=? time 'breakfast) (set-animal-breakfast! animal true)]
+        [(symbol=? time 'dinner) (set-animal-dinner! animal true)]))
+
+(require rackunit)
+(require rackunit/text-ui)
+
+(define feed-animal-tests
+  (test-suite
+   "Test for feed-animal"
+
+   (test-case
+    ""
+    (define monkey (create-animal 'monkey))
+    (feed-animal monkey 'breakfast)
+    (check-equal? monkey (make-animal 'monkey true false))
+    (feed-animal monkey 'dinner)
+    (check-equal? monkey (make-animal 'monkey true true))
+    )
+   ))
+
+(exit (run-tests feed-animal-tests))
