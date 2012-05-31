c5642e3feee258ede8c4c463531d67ca71962ba1
Jeong-Min Lee <falsetru@gmail.com>
2012-05-31 10:38:06 +0900

    41.2.5: Add missing move-circle!.

--- 41/41.2.5.scm
+++ 41/41.2.5.scm
@@ -2,6 +2,7 @@
 
 (define-struct posn (x y) #:mutable #:transparent)
 (define-struct square (nw length) #:mutable #:transparent)
+(define-struct circle (center radius) #:mutable #:transparent)
 ; nw: posn
 ; length: number
 
@@ -16,6 +17,10 @@
   (set-posn-x! (square-nw sq)
                (+ (posn-x (square-nw sq)) delta)))
 
+(define (move-circle! cc delta)
+  (set-posn-x! (circle-center cc)
+               (+ (posn-x (circle-center cc)) delta)))
+
 
 
 (require rackunit)
@@ -26,10 +31,17 @@
    "Test for move-square!"
 
    (test-case
-    "+5"
+    "move-square!"
     (define a-sq (make-square (make-posn 5 6) 10))
     (move-square! a-sq 5)
     (check-equal? a-sq (make-square (make-posn 10 6) 10))
+    )
+
+   (test-case
+    "move-circle!"
+    (define a-cc (make-circle (make-posn 10 10) 5))
+    (move-circle! a-cc 9)
+    (check-equal? a-cc (make-circle (make-posn 19 10) 5))
     )
    ))
 
