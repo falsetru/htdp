a3d6e04b0d7b4596e593e27baa3ee80e81879b59
Jeong-Min Lee <falsetru@gmail.com>
2012-06-08 21:16:24 +0900

    43.2.3

---
+++ 43/43.2.3.scm
@@ -0,0 +1,23 @@
+#lang racket
+
+(require "43.2.1.scm")
+
+(define (how-many-descendants ftn)
+  (cond [(false? ftn) 0]
+        [else
+          (+ 1
+             (foldr + 0 (map how-many-descendants (person-children ftn))))]))
+
+(require rackunit)
+(require rackunit/text-ui)
+
+(define how-many-ancestors-tests
+  (test-suite
+   "Test for how-many-ancestors"
+
+   (check-equal? (how-many-descendants Gustav) 1)
+   (check-equal? (how-many-descendants Carl) 5)
+   (check-equal? (how-many-descendants Eva) 2)
+   ))
+
+(exit (run-tests how-many-ancestors-tests))
