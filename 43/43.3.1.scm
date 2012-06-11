2061f25611ba9454b93ba6b62145e46ab44a13e3
Jeong-Min Lee <falsetru@gmail.com>
2012-06-11 20:47:54 +0900

    43.3.1

---
+++ 43/43.3.1.scm
@@ -0,0 +1,48 @@
+#lang racket
+
+(define (board-for-all f board)
+  (local ((define N (vector-length board)))
+    (for-each (lambda (i)
+      (for-each (lambda (j)
+        (vector-set!
+          (vector-ref board i) j
+          (f i j (vector-ref (vector-ref board i) j))))
+        (build-list N identity)))
+      (build-list N identity))))
+
+(define (place-queen board i j)
+  (board-for-all (lambda (I J x)
+    (cond [(or (= i I)
+               (= j J)
+               (= (+ i j) (+ I J))
+               (= (- i j) (- I J)))
+           false]
+          [else x]))
+    board))
+
+(require rackunit)
+(require rackunit/text-ui)
+
+(define place-queen-tests
+  (test-suite
+   "Test for place-queen"
+
+   (test-case
+    ""
+    (define board
+      (build-vector
+        3
+        (lambda (i)
+          (build-vector
+            3
+            (lambda (j) true)))))
+    (place-queen board 0 0)
+    (check-equal?
+      board
+      '#(#(#f #f #f)
+         #(#f #f #t)
+         #(#f #t #f)))
+    )
+   ))
+
+(exit (run-tests place-queen-tests))
